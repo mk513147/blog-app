36959b1554e425a020dbb25d5e94fe8d84d376d5
Mohit kumar <mk513147@gmail.com>
2024-12-30 12:34:33 +0530

    added tailwind, components and redux

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      I am a footer
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      I am a header
+    </div>
+  )
+}
+
+export default Header
